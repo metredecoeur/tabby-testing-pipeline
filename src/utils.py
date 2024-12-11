ed0fef2d760b462eeeb24caf8477709b1a7effbe
mborek <01169148@pw.edu.pl>
2024-12-11 21:20:31 +0100

    style: formatting and type hints

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
+from pathlib import Path
 
 
-def load_file(path: str) -> str:
+def load_file(path: Path) -> str:
     with open(path, "r") as f:
         content = f.read()
         return content
 
 
-def save_file(path: str, content: str):
+def write_to_file(path: Path, content: str):
     path.parent.mkdir(parents=True, exist_ok=True)
     with open(path, "w") as f:
         f.write(content)
