a066d28eed84c8623fe298ac71805d84257f97d3
mborek <01169148@pw.edu.pl>
2025-01-24 11:59:18 +0100

    refactor: rename file handling connection to Tabby's api

--- src/tabby_connection.py
+++ src/tabby_connection.py
@@ -13,6 +13,11 @@ class TabbyConnection:
     """
 
     def __init__(self, url: str, auth_token: str):
+        """
+        Args:
+            url (str): api url for fetching suggestions
+            auth_token (str): authorization token for Tabby services
+        """
         self._url = url
         self._auth_token = auth_token
         self._session = requests.Session()
@@ -25,7 +30,8 @@ class TabbyConnection:
 
     def _adapt_session(self):
         """
-        _summary_
+        Apply wind-down & retry strategy to HTTP requests
+        encountering server-side issues
         """
         retry_strategy = Retry(
             total=10,
@@ -39,22 +45,17 @@ class TabbyConnection:
     def _prepare_request_data(
         self, language: str, prefix: str, suffix: str = None
     ) -> str:
-        """
-        Prepare body of the post request.
-        Parameters
-        ----------
-        language : str
-            programming language of the prefix
-        prefix : str
-            prefix of the code to get completion for
-        suffix : str, optional
-            suffix of the code to get completion for, by default None
+        """Prepare body of the post request for suggestions endpoint
+        Args:
+            language (str): name of prefix&sufix's programming langauge
+            prefix (str): part of code preceding autocompletion trigger point
+            suffix (str, optional): remaining code after autocompletion trigger point.
+            Defaults to None.
 
-        Returns
-        -------
-        str
-            formatted body of the post request
+        Returns:
+            str: json format of the request body serialized into string
         """
+
         if suffix is None:
             suffix = ""
         request_body = {
@@ -68,12 +69,30 @@ class TabbyConnection:
         return json.dumps(request_body)
 
     def _send_post(self, post_data: str) -> dict:
+        """Send prepared body to Tabby autocompletion endpoint
+
+        Args:
+            post_data (str): json request body serialized to string
+
+        Returns:
+            dict: response content turned to object
+        """
         response = self._session.post(url=self._url, data=post_data, timeout=(60, 120))
         response.raise_for_status()
         response_data = response.json()
         return response_data
 
     def get_suggestion(self, language: str, prefix: str, suffix: str = None) -> dict:
+        """Prepare request and retrieve response from the server
+
+        Args:
+            language (str): name of prefix&sufix's programming langauge
+            prefix (str): _description_
+            suffix (str, optional): _description_. Defaults to None.
+
+        Returns:
+            dict: _description_
+        """
         post_data = self._prepare_request_data(language, prefix, suffix)
         response_data = self._send_post(post_data)
         return response_data
