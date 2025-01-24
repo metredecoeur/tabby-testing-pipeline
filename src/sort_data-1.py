3d7f31eca74b88fe07be2c9845284d1083ad9a5a
mborek <01169148@pw.edu.pl>
2025-01-24 11:54:52 +0100

    refactor & docs: remove cli from data sorting, add documentation

--- src/sort_data-1.py
+++ src/sort_data-1.py
@@ -1,27 +1,62 @@
-import click
 import shutil
 import argparse
 
-
 from pathlib import Path
+from collections.abc import Generator
+
+from utils import get_data_dir
 
 
 class DataSorter:
+    """
+    Utility for sorting raw dataset,
+    by keeping only the files with allowed extensions
+    and removing the ones that are empty
+    """
+
     def __init__(
         self, in_dir_path: Path, out_dir_path: Path, file_extensions: list[str]
     ):
+        """
+        Args:
+            in_dir_path (Path): path to raw/unsorted dataset
+            out_dir_path (Path): destination path to save sorted copy of raw dataset
+            file_extensions (list[str]): allowed extensions of the files to keep
+        """
         self._in_dir_path = in_dir_path
         self._out_dir_path = out_dir_path
         self._file_extensions = file_extensions
 
     def _file_extension_allowed(self, fpath: Path) -> bool:
+        """Check if extension of file is on allowed list
+
+        Args:
+            fpath (Path): filepath to check
+
+        Returns:
+            bool: True for allowed extension, False otherwise
+        """
         file_extension_without_dot = fpath.suffix[1:]
         return any([file_extension_without_dot == ext for ext in self._file_extensions])
 
     def _file_not_empty(self, fpath: Path) -> bool:
+        """Checks if file's contents are not empty
+
+        Args:
+            fpath (Path): filepath to check
+
+        Returns:
+            bool: True for file not empty, False otherwise
+        """
         return fpath.stat().st_size != 0
 
-    def _next_matching_filepath(self) -> Path:
+    def _next_matching_filepath(self) -> Generator[Path]:
+        """Iterates over and yields files from the raw dataset
+         that fit the sorting criteria
+
+        Yields:
+            Generator[Path]: matching filepath
+        """
         for f in self._in_dir_path.rglob("*"):
             if (
                 f.is_file()
@@ -31,35 +66,27 @@ class DataSorter:
                 yield f
 
     def _copy_file_from_to(self, source: Path, destination: Path):
+        """Copy file from source to destination
+
+        Args:
+            source (Path): file's original location
+            destination (Path): destination for file's copy
+        """
         destination.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy2(source, destination, follow_symlinks=False)
 
     def run(self):
+        """Perform sorting by copying for the whole raw database"""
         for fpath in self._next_matching_filepath():
             self._copy_file_from_to(
-                fpath, self._out_dir_path / fpath.relative_to(self._in_dir_path.parent)
+                fpath, self._out_dir_path / fpath.relative_to(self._in_dir_path)
             )
 
 
-@click.command()
-@click.option(
-    "--in-dir-path",
-    required=True,
-    help="path to a directory which contents will be sorted to leave only source code files",
-)
-@click.option(
-    "--out-dir-path",
-    required=True,
-    help="path to a directory where sorted in_dir structure will be saved",
-)
-@click.option(
-    "--file-extension",
-    required=True,
-    multiple=True,
-    help="Extensions of the files to be kept in the sorted codebase passed without leading dots",
-)
-def main(in_dir_path: str, out_dir_path: str, file_extension: list[str]):
-    sorter = DataSorter(Path(in_dir_path), Path(out_dir_path), file_extension)
+def main():
+    raw_db_path: Path = get_data_dir() / "raw"
+    sorted_db_path: Path = get_data_dir() / "sorted"
+    sorter = DataSorter(raw_db_path, sorted_db_path, ["py"])
     sorter.run()
 
 
