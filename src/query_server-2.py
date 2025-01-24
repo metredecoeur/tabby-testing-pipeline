a4fcb5ce3e58b9664507ce14fbb317e041e6f046
mborek <01169148@pw.edu.pl>
2025-01-24 12:02:04 +0100

    docs & feat: add timer for the querying execution, add documentatin

--- src/query_server-2.py
+++ src/query_server-2.py
@@ -1,20 +1,23 @@
 import time
 import os
-import click
+from tqdm import tqdm
+import requests
 from pathlib import Path
 from dotenv import load_dotenv
-from api_connection import TabbyConnection
-from prefix_generator import PrefixGenerator
+from collections.abc import Generator
+
 import const
 import utils
-import requests
+from tabby_connection import TabbyConnection
+from prefix_generator import PrefixGenerator
 
 
 class TabbySuggestionsFetcher:
     """
     Fetches suggestions from the Tabby server
-    for each file's prefix increasing by ratio step,
-    in the source directory
+    for increasingly longer prefixes selected
+    from reference program files using split_ratio_step
+    and saves the autocompleted files to a new location
     """
 
     def __init__(
@@ -24,16 +27,37 @@ class TabbySuggestionsFetcher:
         out_dir_path: Path,
         split_ratio_step: float,
         language: str,
-        verbose: bool = True,
     ):
+        """
+        Args:
+            tabby_connection (TabbyConnection): api connection utility
+            in_dir_path (Path): sorted database
+            out_dir_path (Path): _description_
+            split_ratio_step (float): _description_
+            language (str): _description_
+            verbose (bool, optional): _description_. Defaults to True.
+        """
         self._tabby_connection = tabby_connection
         self._in_dir_path = in_dir_path
         self._out_dir_path = out_dir_path
         self._split_ratio_step = split_ratio_step
         self._language = language
-        self._verbose = verbose
 
-    def _next_filepath(self):
+    def _total_file_count(self) -> int:
+        """
+        Returns:
+            int: total number of files in input directory
+        """
+        total = 0
+        for root, dirs, files in os.walk(self._in_dir_path):
+            total += len(files)
+        return total
+
+    def _next_filepath(self) -> Generator[Path]:
+        """
+        Yields:
+            Generator[Path]: next path from input directory
+        """
         for fpath in self._in_dir_path.rglob("*"):
             if fpath.is_file():
                 yield fpath
@@ -44,20 +68,41 @@ class TabbySuggestionsFetcher:
         and saving new version with completion
         for each prefix
         """
-        for fpath in self._next_filepath():
-            self._log_status(f"file: {fpath}")
+        start = time.perf_counter()
+        for fpath in tqdm(
+            self._next_filepath(),
+            desc="Fetching autocompletions",
+            total=self._total_file_count(),
+            leave=False,
+        ):
+            og_content = utils.load_file(fpath)
             prefix_gen = PrefixGenerator(utils.load_file(fpath), self._split_ratio_step)
-            for ratio, prefix, suffix in prefix_gen.next_prefix():
-                self._log_status(f"ratio: {ratio:.2f}")
-
+            for ratio, prefix, _ in tqdm(
+                prefix_gen.next_prefix(),
+                desc=f"{fpath.parent.name}/{fpath.name}",
+                total=9,
+                leave=False,
+            ):
+                first_suggestion = ""
                 response_data = self._await_request_response(prefix)
                 first_suggestion = response_data["choices"][0]["text"]
-                self._save_tabby_completed_code(fpath, ratio, prefix + first_suggestion)
+                prefix += first_suggestion
+                self._save_tabby_completed_code(fpath, ratio, prefix)
+
+        end = time.perf_counter()
+        benchmark_msg = "Total time: {:.sf}s".format(end - start)
+        tqdm.write("Fetching autocompletions done!")
+        tqdm.write(benchmark_msg)
+        utils.write_to_file(
+            utils.get_data_dir() / "requests_timing.json", benchmark_msg
+        )
 
     def _await_request_response(self, prefix: str):
-        """
-        Accommodates for possible timeouts of the server,
+        """Accommodates for possible timeouts of the server,
         given the potential intensity and frequency of the requests
+
+        Args:
+            prefix (str): prefix to be used for the request
         """
         while True:
             try:
@@ -66,14 +111,16 @@ class TabbySuggestionsFetcher:
                 )
                 return response_data
             except requests.HTTPError as e:
-                self._log_status(f"request error: {e}")
-                self._log_status(f"retrying in {const.REQUESTS_TIMEOUT} seconds")
                 time.sleep(const.REQUESTS_TIMEOUT)
 
     def _save_tabby_completed_code(self, fpath: Path, ratio: float, content: str):
-        """
-        Saves Tabby completed prefix to a file,
+        """Saves Tabby completed prefix to a file,
         ordered in folders by prefix ratio
+
+        Args:
+            fpath (Path): path of currently processed reference file
+            ratio (float): ratio of the current split
+            content (str): prefix generated with ratio, autocompleted by Tabby
         """
         dest_fpath = (
             self._out_dir_path
@@ -81,63 +128,21 @@ class TabbySuggestionsFetcher:
             / fpath.relative_to(self._in_dir_path)
         )
         utils.write_to_file(dest_fpath, content)
-        self._log_status(f"saved: {dest_fpath}")
-
-    def _log_status(self, message):
-        """Prints message to stdout if verbose mode is enabled"""
-        if self._verbose:
-            print(message)
 
 
-@click.command()
-@click.option(
-    "--in-dir-path",
-    required=True,
-    help="path to a directory which contents will be sent to Tabby for suggestions",
-)
-@click.option(
-    "--out-dir-path",
-    required=True,
-    help="path to a directory suggestions from Tabby will be saved",
-)
-@click.option(
-    "--split-ratio-step",
-    required=True,
-    type=click.FLOAT,
-    help="Float value (0,1) for the file split step ratio",
-)
-@click.option(
-    "--language",
-    required=True,
-    help="programming language of the prefix",
-    default="python",
-)
-@click.option("--verbose", help="verbose mode", is_flag=True)
-def main(
-    in_dir_path: str,
-    out_dir_path: str,
-    split_ratio_step: float,
-    language: str,
-    verbose: bool,
-):
+def main():
     load_dotenv()
-    tabby_auth_token = os.environ["tabby_auth_token"]
-
-    try:
-        in_dir_path = Path(in_dir_path).resolve(strict=True)
-        out_dir_path = Path(out_dir_path).resolve(strict=True)
-    except OSError as e:
-        print(e)
-    else:
-        fetcher = TabbySuggestionsFetcher(
-            TabbyConnection(const.TABBY_URL, tabby_auth_token),
-            Path(in_dir_path).resolve(strict=True),
-            Path(out_dir_path).resolve(strict=True),
-            float(split_ratio_step),
-            language,
-            bool(verbose),
-        )
-        fetcher.run()
+    tabby_auth_token = os.getenv("tabby_auth_token")
+    sorted_db_path = utils.get_data_dir() / "sorted"
+    out_dir_path = utils.get_data_dir() / "autocompletions"
+    fetcher = TabbySuggestionsFetcher(
+        TabbyConnection(const.TABBY_URL, tabby_auth_token),
+        sorted_db_path,
+        out_dir_path,
+        const.SPLIT_RATIO_STEP,
+        const.DEFAULT_LANGUAGE,
+    )
+    fetcher.run()
 
 
 if __name__ == "__main__":
